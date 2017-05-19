7998931de14a82bbd3502a99f0c656fb0b465c6e
Jeff Freedman <jdfreedman213@gmail.com>
2017-05-19 13:19:02 -0400

    finished Section 4 Lesson 18

--- Code-Scope-And-Interpolation/Starter/app.js
+++ Code-Scope-And-Interpolation/Starter/app.js
@@ -1,6 +1,13 @@
+/*global angular */
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', function($scope) {
+myApp.controller('mainController', ['$scope', '$timeout', function ($scope, $timeout) {
 
+    "use strict";
+    $scope.name = 'Jeff';
+
+    $timeout(function () {
+        $scope.name = 'Everbody';
+    }, 3000);
 
 }]);
