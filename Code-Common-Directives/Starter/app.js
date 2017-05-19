a6a2f1418082d4de1292ed38875f951928cac378
Jeff Freedman <jdfreedman213@gmail.com>
2017-05-19 14:37:21 -0400

    ng-repeat directive

--- Code-Common-Directives/Starter/app.js
+++ Code-Common-Directives/Starter/app.js
@@ -10,4 +10,9 @@ myApp.controller('mainController', ['$scope', '$filter', function($scope, $filte
 
     $scope.characters = 5;
 
+    $scope.rules = [
+        { rulename: "Must be 5 characters"  },
+        { rulename: "Must not be used elsewhere"},
+        { rulename: "Must be cool"}
+    ];
 }]);
