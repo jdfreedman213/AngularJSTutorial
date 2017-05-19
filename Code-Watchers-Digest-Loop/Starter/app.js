36547c2341907f6dcf7aac9205460c6b6d4e48e5
Jeff Freedman <jdfreedman213@gmail.com>
2017-05-19 13:57:30 -0400

    Completed Section 4 Lesson 20

--- Code-Watchers-Digest-Loop/Starter/app.js
+++ Code-Watchers-Digest-Loop/Starter/app.js
@@ -1,6 +1,18 @@
-var tb = document.getElementById("name");
+/*global angular, console */
+var myApp = angular.module('myApp', []);
 
-tb.addEventListener("keypress",
-    function(event) {
-        console.log("Pressed!");
+myApp.controller('mainController', ['$scope', '$filter', function ($scope, $filter) {
+    "use strict";
+
+    $scope.handle = '';
+    $scope.toLower = function (str) {
+        return $filter('lowercase')(str);
+    };
+
+    $scope.$watch('handle', function (newValue, oldValue) {
+        console.info('Changed!');
+        console.log('Old: ' + oldValue);
+        console.log('New: ' + newValue);
+
     });
+}]);
